46d6a31855da956bf38cfde843a181c2a57f4f0e
Loko24 <epastor@sanmateo.edu.co>
2019-07-17 12:20:20 -0500

    Se cargan métodos de borrar un nodo, insertar un nodo e insertar un nodo al inicio y se hacen ejempls de cada uno de ellos

--- Lista_Ligada/Lista_Ligada/CLIsta_Ligada.cs
+++ Lista_Ligada/Lista_Ligada/CLIsta_Ligada.cs
@@ -126,5 +126,106 @@ namespace Lista_Ligada
             //Si no se encontró el pDato retorna null indicando que no está
             return null;
         }
+
+        //Buscar indice, es decir que va a mostrar la ubicación del nodo que se va a buscar
+        //Es similar a los arreglos con las posiciones
+        //El indice -1 es una información que el dato no fue encontrado
+        public int BuscarIndice(int pDato)
+        {
+            //Se asigna -1 a n esto se hace porque trabajo está iniciando en el ancla
+            //En donde el ancla inicia antes del primer nodo
+            int n = -1;
+            trabajo = ancla;
+            while(trabajo.Siguiente != null)
+            {
+                trabajo = trabajo.Siguiente; 
+                //Se mueve en proporción al recorrido de trabajo, es decir nodo a nodo
+                n++;
+                //Si, el valor que tiene trabajo es igual al solicitado "pDato"
+                //Retorna el valor de n, es decir del indicedionde se ubica el dato
+                if (trabajo.Dato == pDato)
+                {
+                    return n;
+                }
+            }
+            //Si no se encontró el pDato retorna -1 indicando que no está
+            return -1;
+        }
+
+        //Encuentra el nodo anterior al ingresado
+        //Si es el primero retorna el ancla
+        //Si el dato no existe trae el último nodo
+        public CNodo BuscarAnterior(int pDato)
+        {
+            //Trabajo2 va a ser igual al ancla para comenzar la busqueda
+            trabajo2 = ancla;
+            //Mientras trabajo2 sea diferente de null y el nodo siguiente sea diferente a pDato entonces entra al cilo
+            while(trabajo2.Siguiente != null && trabajo2.Siguiente.Dato != pDato)
+            {
+                //Asigna el nodo a trabajo2
+                trabajo2 = trabajo2.Siguiente;
+            }
+            return trabajo2;
+        }
+
+        //Borrar la primera ocurrencia del dato suministrado
+        public void Borrar(int pDato)
+        {
+            //Se verifica que hayan datos dentro de los nodos de la lista
+            if(EstaVacio() == true)
+            {
+                return;
+            }
+            //Se buscan los nodos con los que se quiere trabajar, es decir los que contienen el valor de pDato
+            //La idea es tener una referencia al nodo del dato y al nodo anterior
+            CNodo anterior = BuscarAnterior(pDato);
+            CNodo encontrar = Buscar(pDato);
+            //Si no hay nodos para borrar salimos de la función
+            if (encontrar == null)
+            {
+                return;
+            }
+            //Brincamos el nodo, es decir que lo omitimos
+            //Lo que hace es conectar el nodo anterior al dato buscado con el nodo siguiente al nodo del dato buscadi
+            anterior.Siguiente = encontrar.Siguiente;
+            //Como se "saltó" el nodo encontrado, es posible retirarlo de la lista
+            encontrar.Siguiente = null;
+        }
+
+        //Inserta el valor que se quiere despupés de la ocurrencia del dato pasado a pDonde
+        //pDonde es el dato del nodo desde el cual queremos que se haga la insersión
+        //pValor es el dato que se va a insertar en el nuevo nodo
+        public void Insertar(int pDonde, int pValor)
+        {
+            //Se busca y encuentra la posición en donde se desea insertar el dato
+            trabajo = Buscar(pDonde);
+            //Se hace una verificación para saber si existe el lugar donde se quiere insertar
+            if(trabajo == null)
+            {
+                return;
+            }
+            //Se crea el nodo temporal que se quiere insertar
+            CNodo temp = new CNodo();
+            temp.Dato = pValor;
+            //Se conecta temp a la lista, es decir que se hace una referencia al valor siguiente
+            temp.Siguiente = trabajo.Siguiente;
+            //Se conecta trabajo al temp
+            trabajo.Siguiente = temp;
+        }
+        //Inserta el valor que se quiere despupés del ancla
+        //pDonde es el dato del nodo desde el cual queremos que se haga la insersión
+        //pValor es el dato que se va a insertar en el nuevo nodo
+        public void InsertarAlInicio( int pValor)
+        {
+            //Se asigna el valor del ancla a trabajo
+            trabajo = ancla;
+            //Se crea el nodo temporal que se quiere insertar
+            CNodo temp = new CNodo();
+            temp.Dato = pValor;
+            //Se conecta temp a la lista, es decir que se hace una referencia al valor siguiente
+            temp.Siguiente = trabajo.Siguiente;
+            //Se conecta trabajo al temp
+            trabajo.Siguiente = temp;
+        }
     }
 }
--- Lista_Ligada/Lista_Ligada/Program.cs
+++ Lista_Ligada/Lista_Ligada/Program.cs
@@ -27,6 +27,10 @@ namespace Lista_Ligada
             //y retorna a encontrado dependiendo su resultado
             CNodo encontrado = miLista.Buscar(15);
             Console.WriteLine(encontrado);
+            //Dentro de la impresión se asigna la función y el parametro a encontrar con su indice
+            Console.WriteLine(miLista.BuscarIndice(9));
+            //Dentro de la impresión se asigna la función y el parametro a encontrar con el nodo anterior al solicitado
+            Console.WriteLine(miLista.BuscarAnterior(9));
             //Desocupa la lista y luego inserna un nuevo nodo
             miLista.Vaciar();
             miLista.Adicionar(15);
@@ -38,6 +42,24 @@ namespace Lista_Ligada
             miLista.Transversa();
             //Retorna true indicando que no hay un nodo, ya que el ancla apunta a null
             Console.WriteLine(miLista.EstaVacio());
+            //Inserta diferentes nodos con su respectivo valor
+            miLista.Adicionar(3);
+            miLista.Adicionar(5);
+            miLista.Adicionar(7);
+            miLista.Adicionar(9);
+            miLista.Adicionar(11);
+            miLista.Adicionar(15);
+            //Recorre y muestra los objetos de la lista
+            miLista.Transversa();
+            //Borra el nodo que contiene el valor 9
+            miLista.Borrar(9);
+            //Recorre y muestra los objetos de la lista
+            miLista.Transversa();
+            //Inserta un nodo que contiene a 2 y que será insertado después del nodo que contiene a 15
+            miLista.Insertar(15,2);
+            //Inserta un nodo después del ancla que contiene un valor
+            miLista.InsertarAlInicio(9455);
+            miLista.Transversa();
         }
     }
 }
